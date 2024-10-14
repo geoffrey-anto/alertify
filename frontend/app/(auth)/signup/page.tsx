7b9560e52cba448043a6f34379cf4038c237a5c8
geoffrey-anto <geoffreyanto.ignatiuse2021@vitstudent.ac.in>
2024-10-15 02:20:18 +0530

    init

---
+++ frontend/app/(auth)/signup/page.tsx
@@ -0,0 +1,8 @@
+import { SignUp } from "@/components/signup";
+import React from "react";
+
+const Page = () => {
+  return <SignUp />;
+};
+
+export default Page;
---
+++ frontend/components/login.tsx
@@ -0,0 +1,104 @@
+"use client";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import api from "@/lib/api";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/hooks/use-toast";
+
+export const description =
+  "A login form with email and password. There's an option to login with Google and a link to sign up if you don't have an account.";
+
+export function Login() {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = new FormData(event.currentTarget);
+    const email = form.get("email") as string;
+    const password = form.get("password") as string;
+
+    const payload = {
+      email,
+      pass: password,
+    };
+
+    try {
+      const response = await api.post("/login", payload, {
+        withCredentials: true,
+      });
+
+      if (response.status === 200) {
+        router.push("/dashboard");
+      } else {
+        toast({
+          title: "Error",
+          description: "Invalid email or password",
+          variant: "destructive",
+        });
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "An error occurred while logging in",
+        variant: "destructive",
+      });
+    }
+  };
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form className="grid gap-4" onSubmit={handleSubmit}>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link href="#" className="ml-auto inline-block text-sm underline">
+                Forgot your password?
+              </Link>
+            </div>
+            <Input name="password" id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <Button variant="outline" className="w-full">
+            Login with Google
+          </Button>
+        </form>
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <Link href="/signup" className="underline">
+            Sign up
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
